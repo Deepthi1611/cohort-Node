833177c94ba756367bc82353b6c68906fb82d3ed
Deepthi <deepthipurijala@gmail.com>
2026-01-23 16:49:27 +0530

    postgresql connection

---
+++ week-17-postgres/src/index.ts
@@ -0,0 +1,27 @@
+import { Client } from 'pg';
+
+// using connection string
+const pgClient = new Client("psql 'postgresql://neondb_owner:npg_Q2zrsWI7fKRH@ep-polished-rain-ahbmxeci-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'")
+
+// using config object
+const pgClient2 = new Client({
+    user: 'neondb_owner',
+    password: 'npg_Q2zrsWI7fKRH',
+    host: 'ep-polished-rain-ahbmxeci-pooler.c-3.us-east-1.aws.neon.tech',
+    port: 5432,
+    database: 'neondb',
+    ssl: true
+});
+
+// this is an async operation
+async function main() {
+    await pgClient2.connect();
+}
+
+// pgclient.query is used to execute SQL queries
+// pgclient.query returns a promise
+// we can use await to wait for the promise to resolve
+// const res = await pgClient.query('SELECT NOW()');
+// console.log(res.rows); // print the result rows
+
+main();
