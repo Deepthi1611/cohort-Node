30c22616c52bf5e74ddca1a6ff4d3de85ab6fc4b
Deepthi <deepthipurijala@gmail.com>
2026-02-03 23:10:37 +0530

    added broadcast, FE

--- week-16.2-chatapp/src/index.ts
+++ week-16.2-chatapp/src/index.ts
@@ -1,16 +1,40 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const sockets = new WebSocketServer({ port: 8080 });
 
 // whenever a client connects, this connection gets called with a websocket object 
 // which lets us communicate with that specific client
 // web sockets don't have methods like get and post, instead they have events like message, open, close, error
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
-wss.on("connection", (ws) => {
+// when a client connects
+sockets.on("connection", (socket: WebSocket) => {
     userCount++;
+    allSockets.push(socket);
     console.log('user connected, total users:', userCount); 
+
+    // when we receive a message from a client
+    socket.on("message", (message) => {
+        // when we receive a message from a client, we want to broadcast it to all connected clients
+        console.log("received:", message.toString());
+        // sending message back to the same client
+        // socket.send(`Sent from the server: ${message.toString()}`);
+
+        // send the message to all connected clients
+        allSockets.forEach((s) => {
+            s.send(`User says: ${message.toString()}`);
+        });
+    });
+
+    // when a client disconnects
+    socket.on("disconnect", () => {
+        userCount--;
+        allSockets = allSockets.filter(s => s !== socket);
+        console.log('user disconnected, total users:', userCount); 
+    });
+
 });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+console.log("WebSocket server is running on socket://localhost:8080");
