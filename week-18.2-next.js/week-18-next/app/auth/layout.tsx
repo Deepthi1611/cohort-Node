2b9e54cdc05bef2b22785e093eec472102e6be2b
Deepthi <deepthipurijala@gmail.com>
2026-03-07 23:44:47 +0530

    async component and loading

--- week-18.2-next.js/week-18-next/app/auth/layout.tsx
+++ week-18.2-next.js/week-18-next/app/auth/layout.tsx
@@ -6,3 +6,10 @@ export default function layout({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
+
+// if we want to use the layout page for all the pages in the auth folder, 
+// we can create a layout.tsx file in the auth folder and export the layout component from there. 
+// This way, we don't have to import the layout component in each page and wrap the page component with it. 
+// Next.js will automatically use the layout component for all the pages in the auth folder and the path would be /auth/login, /auth/register, etc.
+// if we don't want the path to be /auth/login, /auth/register, etc. and we want it to be /login, /register, etc. 
+// then we can create a layout.tsx file in the auth folder as (auth) instead of auth and export the layout component from there.
--- week-18.2-next.js/week-18-next/app/users/page.tsx
+++ week-18.2-next.js/week-18-next/app/users/page.tsx
@@ -1,7 +1,25 @@
-export default function Users() {
+// server component
+// Browser Network tab only shows browser-made calls, not server-to-server calls.
+// api calls made in server components are not visible in the browser's network tab,
+// but they are visible in the terminal where the Next.js server is running.
+
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return data;
+}
+
+export default async function Users() {
+  const users = await getUsers();
   return (
     <div>
       <h1>users</h1>
+      {users.map((user: any) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
