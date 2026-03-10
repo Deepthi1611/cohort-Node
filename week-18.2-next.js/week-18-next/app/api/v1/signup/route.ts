afa043a6572cff39ca953ec73ec5a66f1eeae8a4
Deepthi <deepthipurijala@gmail.com>
2026-03-10 19:57:51 +0530

    added db and prisma

--- week-18.2-next.js/week-18-next/app/api/v1/signup/route.ts
+++ week-18.2-next.js/week-18-next/app/api/v1/signup/route.ts
@@ -1,19 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/app/lib/db";
 
 // request is the request object whose type is NextRequest, 
 // which is an extension of the standard Request object with additional Next.js-specific properties and methods.
 // It provides information about the incoming HTTP request, such as headers, body, query parameters, and more. You can use this object to access data sent by the client when making a POST request to the /api/v1/signup endpoint.
 export async function POST(request: NextRequest) {
-    // The line request.json() is used to parse the JSON body of the incoming HTTP request. 
-    // It returns a promise that resolves to the parsed JSON data. 
-    // The .catch(() => ({})) part is a way to handle any potential errors that might occur during the parsing process. 
-    // If the parsing fails (for example, if the request body is not valid JSON), 
-    // it will catch the error and return an empty object instead. 
-    // This ensures that the code can continue executing without crashing due to a parsing error.
-    const data = await request.json().catch(() => ({}));
-    console.log("Received signup data:", data);
+  // The line request.json() is used to parse the JSON body of the incoming HTTP request. 
+  // It returns a promise that resolves to the parsed JSON data. 
+  // The .catch(() => ({})) part is a way to handle any potential errors that might occur during the parsing process. 
+  // If the parsing fails (for example, if the request body is not valid JSON), 
+  // it will catch the error and return an empty object instead. 
+  // This ensures that the code can continue executing without crashing due to a parsing error.
+  const data = await request.json().catch(() => ({}));
+
+    if (!data.name || !data.email || !data.password) {
+        return NextResponse.json(
+            { message: "name, email and password are required" },
+            { status: 400 }
+        );
+    }
+    await prisma.user.create({
+        data: {
+            name: data.name,
+            email: data.email,
+            password: data.password,
+        },
+    });
+
   return NextResponse.json(
-    { message: "Signup successful (not really, this is a mock API)." },
+    { message: "Signup successful" },
     { status: 201 }
   );
 }
